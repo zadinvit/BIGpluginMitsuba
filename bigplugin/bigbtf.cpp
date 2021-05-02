5be8daca93ca9a9bf9de0157a3412bdb3b0e0157
zadinvit <vitek.zadina@gmail.com>
2021-05-02 18:11:17 +0200

    change licence

--- bigplugin/bigbtf.cpp
+++ bigplugin/bigbtf.cpp
@@ -1,3 +1,16 @@
+/**
+* BigBTF plugin - plugin for Mitsuba renderer - version 1.0
+* --------------------------------------------------------
+* Copyright (C) 2021, Vít Zadina (vitek.zadina@gmail.com)
+* Report bugs and download new versions at https://github.com/zadinvit/BIGpluginMitsuba
+*
+* This library is distributed under the 3-clause BSD license. See notice at the end
+* of this file.
+*
+* This library uses parts of MIF Library, which are:
+* Copyright (C) 2021, by  Radomir Vavra (vavra.radomir@gmail.com) and Jiri Filip.
+*/
+
 #include <mitsuba/core/properties.h>
 #include <mitsuba/core/spectrum.h>
 #include <mitsuba/core/string.h>
@@ -306,3 +319,35 @@ protected:
 MTS_IMPLEMENT_CLASS_VARIANT(BigBTF, BSDF)
 MTS_EXPORT_PLUGIN(BigBTF, "bigbtf plugin")
 NAMESPACE_END(mitsuba)
+
+/**
+* 3-Clause BSD License
+*
+* Copyright (c) 2021, Vít Zadina (vitek.zadina@gmail.com).
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* * Neither the name of the copyright holder nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
